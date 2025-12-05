c4f433d2365c7d3014daff97897d804fefb69986
GabrielGaudi <162371856+GabrielGaudi@users.noreply.github.com>
2025-12-05 16:55:13 -0300

    Delete Inventory directory

--- Inventory/Interactable.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public abstract class Interactable : MonoBehaviour
-{
-    [SerializeField] protected string actionMessage;
-    [SerializeField] protected string lookMessage;
-    [SerializeField] protected string failMessage;
-
-}
